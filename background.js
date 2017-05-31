9ed0d27e175df5e8ee466fd7612a4de77cbfeec7
Max Glassie <msglassie@gmail.com>
2017-05-31 15:22:35 -0600

    cleans up commented code

--- background.js
+++ background.js
@@ -1,14 +1,4 @@
-
-// function getSelected(){
-//   return window.getSelection().toString()
-// }
-// console.log(window.getSelection().toString())
-
-// var getSelected = chrome.tabs.executeScript( function(){
-//   return window.getSelection().toString()
-// });
-// 
-
+//creates the right click option
 chrome.runtime.onInstalled.addListener(function() {
   var context = "selection";
   var title = "The Howler - Analyze Sentiment!";
@@ -25,21 +15,3 @@ function onClickHandler(info, tab) {
   var url = 'http://localhost:3000/extension?text=' + encodeURIComponent(sText);
   window.open(url, '_blank');
 };
-
-// var text = getSelected()
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   var url = 'https://the-howler.herokuapp.com/' + encodeURI(getSelected())
-//   chrome.tabs.create({'url': url})
-// });
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
-//      sendServiceRequest(response.data);
-//   });
-// });
-//
-// function sendServiceRequest(selectedText) {
-//   var serviceCall = 'http://www.google.com/search?q=' + selectedText;
-//   chrome.tabs.create({url: serviceCall});
-// }
