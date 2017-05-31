e5f55d2c8c360ccf4dbc912e1bc43baa6a1868f1
Max Glassie <msglassie@gmail.com>
2017-05-31 14:53:06 -0600

    sends params from right clicked text
    to an API endpoint on the application. the endpoint is not built yet.

--- background.js
+++ background.js
@@ -1,19 +1,37 @@
 
-function getSelected(){
-  return window.getSelection().toString()
-}
+// function getSelected(){
+//   return window.getSelection().toString()
+// }
 // console.log(window.getSelection().toString())
 
 // var getSelected = chrome.tabs.executeScript( function(){
 //   return window.getSelection().toString()
 // });
+// 
 
-var text = getSelected()
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var url = 'https://the-howler.herokuapp.com/' + encodeURI(getSelected())
-  chrome.tabs.create({'url': url})
+chrome.runtime.onInstalled.addListener(function() {
+  var context = "selection";
+  var title = "The Howler - Analyze Sentiment!";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "id": "context" + context});
 });
+
+// add click event
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+  var sText = info.selectionText;
+  var url = 'http://localhost:3000/extension?text=' + encodeURIComponent(sText);
+  window.open(url, '_blank');
+};
+
+// var text = getSelected()
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   var url = 'https://the-howler.herokuapp.com/' + encodeURI(getSelected())
+//   chrome.tabs.create({'url': url})
+// });
 
 // chrome.browserAction.onClicked.addListener(function(tab) {
 //   chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
